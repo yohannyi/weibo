620c9784ae418cbdee5aa898e3c20d5264b7d8bc
yohannyi <390657063@qq.com>
2021-05-31 07:26:09 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UserController@create')->name('signup');
 // use Illuminate\Support\Facades\Route;
 
 /*
