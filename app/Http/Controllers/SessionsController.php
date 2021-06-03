bdb1d73961956a447a9381effcc3bc05b5f67d74
yohannyi <390657063@qq.com>
2021-06-03 11:15:11 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -13,6 +13,14 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
+
     public function store(Request $request)
     {
        $credentials = $this->validate($request, [
@@ -28,8 +36,7 @@ class SessionsController extends Controller
         return redirect()->back()->withInput();
     }
 
-
-
-       
-    }
+    
+   }  
+    
 }
